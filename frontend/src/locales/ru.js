11c1d74c9dcb33767ca36a48de4b8975516778cd
oggera <german.nasyrov.95@mail.ru>
2023-04-23 16:27:53 +0300

    add ru locale

---
+++ frontend/src/locales/ru.js
@@ -0,0 +1,27 @@
+export default {
+  translation: {
+    errors: {
+      server: 'Неполадки с сервером',
+      loginValidation: 'Неверные имя пользователя или пароль',
+    },
+    mainPage: {
+      secretChat: 'Secret Chat',
+      signOut: 'Выйти',
+    },
+    loginPage: {
+      header: 'Вход',
+      loginButton: 'Войти',
+      footerMessage: 'Нет аккаунта? ',
+      registrationLink: 'Регистрация',
+      placeholders: {
+        username: 'Ваш ник',
+        password: 'Пароль',
+      },
+    },
+    pageNotFound: {
+      header: 'Страница не найдена',
+      redirectMessage: 'Но вы можете перейти ',
+      redirectLink: 'На главную страницу',
+    },
+  },
+};
