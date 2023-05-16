5c2264b1afd8954aa5279f63bc6b5a181fa4bea1
oggera <german.nasyrov.95@mail.ru>
2023-05-16 09:17:44 +0300

    add bootstrap and make init function to lower case

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import { io } from 'socket.io-client';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import 'react-toastify/dist/ReactToastify.css';
-import Init from './init.js';
+import init from './init.js';
 
 const app = async () => {
   const root = ReactDOM.createRoot(document.querySelector('#root'));
   const socket = io();
-  const vdom = await Init(socket);
+  const vdom = await init(socket);
   root.render(vdom);
 };
 
